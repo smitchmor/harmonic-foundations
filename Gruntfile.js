5044f61ea095fd0ad1b4ce4e40c5a217a4bfdc6a
Sean Moriarty <smoriart@advance.net>
2017-02-03 16:39:25 -0500

    upload gruntfile.js to run web server

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,22 +16,3 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.registerTask('default', ['connect']);
 };
-
-// module.exports = function(grunt) {
-
-// grunt.initConfig({
-// 	connect: {
-// 		server: {
-// 			options: {
-// 				port: 80,
-// 				base: '.',
-// 				keepalive: false,
-// 				hostname: '*'
-// 			}
-// 		}
-// 	});
-
-// // 	grunt.registerTask('rs', ['connect:server:keepalive']);
-// 	grunt.registerTask('default', ['connect']);
-
-// };
